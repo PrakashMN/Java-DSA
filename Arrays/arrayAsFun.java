70d7a0a2f48c51f078f7bea92640d6e3f385e7c8
PrakashMN <nagaralprakash0@gmail.com>
2024-10-18 01:33:16 +0530

    completed code

--- Arrays/arrayAsFun.java
+++ Arrays/arrayAsFun.java
@@ -1,7 +1,16 @@
-package Arrays;
-
 public class arrayAsFun {
+
+    public static void update(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            arr[i]=arr[i]+1;
+        }
+
+    for(int i=0;i<arr.length;i++){
+        System.out.println(arr[i]);
+    }
+    }
     public static void main(String[] args) {
-        
+        int marks[]={97,98,99};
+        update(marks);
     }
 }
