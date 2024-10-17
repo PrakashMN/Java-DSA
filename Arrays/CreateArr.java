08ea37552889d27c884b063be84e53933041d475
PrakashMN <nagaralprakash0@gmail.com>
2024-10-17 21:47:59 +0530

    initial commit

---
+++ Arrays/CreateArr.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class CreateArr {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int marks[]=new int[10];
+        int numbers[]={5,8,12,15,17};
+        String fruits[]=new String[5];
+        String get[]={"jfjg","ojfgh","hdgfjh"};
+    }
+}
