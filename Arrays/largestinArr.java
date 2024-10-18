8d204769e052b69c8b3a5a65fb2028e571b3bc71
PrakashMN <nagaralprakash0@gmail.com>
2024-10-19 00:35:23 +0530

    initial commit

---
+++ Arrays/largestinArr.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class largestinArr {
+   public largestinArr() {
+   }
+
+   public static void input(int[] var0) {
+      Scanner var1 = new Scanner(System.in);
+
+      for(int var2 = 0; var2 < var0.length; ++var2) {
+         var0[var2] = var1.nextInt();
+      }
+
+   }
+
+   public static int large(int[] var0) {
+      int var1 = -9999999;
+
+      for(int var2 = 0; var2 < var0.length; ++var2) {
+         if (var0[var2] > var1) {
+            var1 = var0[var2];
+         }
+      }
+
+      return var1;
+   }
+
+   public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      System.out.println("Enter the length of Array");
+      int var2 = var1.nextInt();
+      int[] var3 = new int[var2];
+      System.out.println("Enter numbers in Array");
+      input(var3);
+      System.out.println("The Largest Number in the Array is : " + large(var3));
+   }
+}
