5cc76b6507e37d91ea63d4ed37109126a3aff18b
PrakashMN <nagaralprakash0@gmail.com>
2024-10-20 03:02:29 +0530

    Addes total no of Subarrays

--- Arrays/printSubarray.java
+++ Arrays/printSubarray.java
@@ -2,6 +2,7 @@
 
 public class printSubarray {
     public static void printSubArr(int arr[]){
+        int ts=0;
         for(int i=0;i<arr.length;i++){
             int start=i;
             for(int j=i;j<arr.length;j++){
@@ -9,10 +10,15 @@ public class printSubarray {
                 for(int k=start;k<=end;k++){
                     System.out.println(arr[k]+" ");
                 }
+                ts++;
                 System.out.println();
             }
+            int subsum=i;
+            System.out.println(subsum);
             System.out.println();
         }
+
+        System.out.println("Total Subarrays : "+ts);
     }
     public static void main(String[] args) {
         int amr[]={2,4,6,8,10};
