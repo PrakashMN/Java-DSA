8ea7d51c3b3bb14ce29fb3d8878562475ae96894
PrakashMN <nagaralprakash0@gmail.com>
2024-10-28 23:42:06 +0530

    Resolved

--- Sorting/selectionSort.java
+++ Sorting/selectionSort.java
@@ -2,7 +2,7 @@
 
 public class selectionSort {
     public static void Ssort(int arr[]){
-        for(int i=0; i<arr.length-1; i++){
+        for(int i=0; i<arr.length; i++){
             int minPos=i;
             for(int j=i+1;j<arr.length;j++){
                 if(arr[minPos]>arr[j]){
