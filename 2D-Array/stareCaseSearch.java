c451fee3cd550dbe7d298a9b8b120286a4c707bf
PrakashMN <nagaralprakash0@gmail.com>
2024-11-02 23:24:27 +0530

    Added Info

--- 2D-Array/stareCaseSearch.java
+++ 2D-Array/stareCaseSearch.java
@@ -1,3 +1,9 @@
+/* Staircase search is one of the efficient searching algorithms to find a key value in a given 2-dimensional array.
+ In this, we first move to the last element of the first row and check if the key is less than the last element then we move to the left of the last element.
+ If the key is greater than the last element then we move to the next row the last element and again check if the key is less than the last element.
+ If we found a key equals to the element of the array then we stop searching further and print the element.
+ This algorithm leads to reduces a lot of time as compared to the normal searching algorithm in the array. */
+
 public class stareCaseSearch {
     public static boolean stareCase(int arr[][],int key){
         int row=0, col = arr[0].length-1;
