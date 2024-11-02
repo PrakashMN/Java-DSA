dc4881b91a4d4d3be64778d866e31538ec5785ed
PrakashMN <nagaralprakash0@gmail.com>
2024-11-03 03:08:50 +0530

    Final Update

--- Arrays/CreateArr.java
+++ Arrays/CreateArr.java
@@ -1,12 +1,27 @@
 import java.util.Scanner;
 
 public class CreateArr {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int marks[]=new int[10];
-        int numbers[]={5,8,12,15,17};
-        String fruits[]=new String[5];
-        String get[]={"jfjg","ojfgh","hdgfjh"};
+    public static void input(int[] arr) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter elements one by one");
+  
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
         sc.close();
+     }
+
+     public static void printarr(int arr[]){
+        System.out.println("Array");
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+     }
+    public static void main(String[] args) {
+        int marks[]=new int[5];
+        input(marks);
+        printarr(marks);
+        int numbers[]={5,8,12,15,17};
+        printarr(numbers);
     }
 }
