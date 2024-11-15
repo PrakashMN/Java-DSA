46b656ebc7bded3e729271b41e2916a39329dc31
PrakashMN <nagaralprakash0@gmail.com>
2024-11-15 23:58:59 +0530

    Initial Commit

---
+++ 05.BitManipulation/basic.java
@@ -0,0 +1,10 @@
+public class basic {
+    public static void main(String[] args) {
+        System.out.println(5&6); 
+        System.out.println(5|6);
+        System.out.println(5^6);
+        System.out.println(~5);
+        System.out.println(~0);
+        System.out.println(~1);
+    }
+}
