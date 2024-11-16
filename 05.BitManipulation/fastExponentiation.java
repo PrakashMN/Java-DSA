53a376b2f1790ce46da9879642fc4e52b4c304be
PrakashMN <nagaralprakash0@gmail.com>
2024-11-17 04:16:10 +0530

    Initial Commit

---
+++ 05.BitManipulation/fastExponentiation.java
@@ -0,0 +1,20 @@
+// This method finds the Exponential of given numbers in Less time complexity
+
+public class fastExponentiation {
+    public static int FastExponentiation(int a, int n){
+        int ans = 1;
+
+        while (n>0) {
+            if((n&1)!=0){
+                ans = ans *a;
+            }
+            a=a*a;
+            n = n>>1;
+        }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.println(FastExponentiation(2, 3));
+    }
+}
