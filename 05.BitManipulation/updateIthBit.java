4adcdf3c0175bbf48e3c5338d32092f75bcada54
PrakashMN <nagaralprakash0@gmail.com>
2024-11-17 02:33:55 +0530

    Initial Commit

--- 05.BitManipulation/updateIthBit.java
+++ 05.BitManipulation/updateIthBit.java
@@ -10,14 +10,20 @@ public class updateIthBit {
         return n|bitMask;
     }
     public static int updateithbit(int n,int i,int newBit){
-        if(newBit==0){
+        // SOLUTION 1
+        /* if(newBit==0){
             return clearithbit(n, i);
         }
         else{
             return setithbit(n,i);
-        }
+        } */
+
+        // SOLUTION 2
+        n = clearithbit(n, i);
+        int bitMask = newBit<<i;
+        return n|bitMask; 
     }
     public static void main(String[] args) {
-    System.out.println(updateithbit(5, 2, 0));
+    System.out.println(updateithbit(10, 2, 1));
     }
 }
