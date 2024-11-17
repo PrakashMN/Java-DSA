0b39b0fba165ad051562ef7d0f2e65f9f01ee4e8
PrakashMN <nagaralprakash0@gmail.com>
2024-11-17 22:51:37 +0530

    Updation

--- Arrays/arrayAsFun.java
+++ Arrays/arrayAsFun.java
@@ -4,10 +4,9 @@ public class arrayAsFun {
         for(int i=0;i<arr.length;i++){
             arr[i]=arr[i]+1;
         }
-
-    for(int i=0;i<arr.length;i++){
-        System.out.println(arr[i]);
-    }
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
     }
     public static void main(String[] args) {
         int marks[]={97,98,99};
