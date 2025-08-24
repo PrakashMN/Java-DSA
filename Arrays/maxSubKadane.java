c128a0d590241e82ac0475c90c1ad16757852f6a
unknown <nagaralprakash0@gmail.com>
2025-08-24 09:23:56 +0530

    Final Commit

--- Arrays/maxSubKadane.java
+++ Arrays/maxSubKadane.java
@@ -1,32 +1,20 @@
 //THIS PROGRAM IS USED TO PRINT THE TOTAL  SUM OF THE SUBARRAYS ARE PRESENT IN THE GIVEN ARRAY USING BRUTE FORCE TECHNIQUE
 
-import java.util.Scanner;
-
 public class maxSubKadane {
-    public static void input(int arr[]){
-        Scanner sc=new Scanner(System.in);
-        for(int i=0;i<arr.length;i++){
-            arr[i]=sc.nextInt();
-        }
-        sc.close();
-    }
-    public static void kadanes(int arr[]){
-        int ms=Integer.MIN_VALUE;
+    public static void kadane(int num[]){
         int cs = 0;
-        for(int i=0;i<arr.length;i++){
-            cs+=arr[i];
+        int ms = 0;
+        for(int i=0;i<num.length;i++){
+            cs = cs+num[i];
             if(cs<0){
                 cs=0;
             }
-            ms= Math.max(cs,ms);
+            ms =Math.max(cs, ms);
         }
-
-        System.out.println("Maximum Sub Array Sum : "+ms);
+        System.out.println("SubSum is : "+ms);
     }
     public static void main(String[] args) {
-        int numbers[]=new int[55];
-        System.out.println("Enter the Array numbers : ");
-        input(numbers);
-        kadanes(numbers);
+        int arr[] ={-2,-3,4,-1,-2,1,5,-3};
+        kadane(arr);
     }
 }
