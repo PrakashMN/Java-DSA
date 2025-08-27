d347d43d5ab806144c9bffb9a5346e02f023feb0
unknown <nagaralprakash0@gmail.com>
2025-08-27 23:58:43 +0530

    Initial Commit

---
+++ Practice/buySellStock.java
@@ -0,0 +1,14 @@
+public class buySellStock {
+
+    public static int buysell(int[] price){
+        int bp = Integer.MAX_VALUE;
+        int mp = 0;
+        for(int i=0;i<price.length;i++){
+            if(bp)
+        }
+    }
+    public static void main(String[] args) {
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println(buysell(prices));
+    }
+}
