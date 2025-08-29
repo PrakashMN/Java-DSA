f6290373ff4cd02dc8476259beed72bfbf17aed0
unknown <nagaralprakash0@gmail.com>
2025-08-30 03:48:29 +0530

    Final Commit

---
+++ Practice/SquresofSortedArray.java
@@ -0,0 +1,36 @@
+/* 
+        977. Squares of a Sorted Array
+
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+ 
+
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+  */
+
+
+import java.util.*;
+
+public class SquresofSortedArray {
+    public static int[] sortedSqures(int nums[]){
+        int arr[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            arr[i]=nums[i]*nums[i];
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+    public static void main(String[] args) {
+        int nums[]={-4,-1,0,3,10};
+        System.out.println(Arrays.toString(sortedSqures(nums)));
+    }
+}
