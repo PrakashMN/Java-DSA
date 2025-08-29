737aaab69338331a8999746482c24f4e9a70901a
unknown <nagaralprakash0@gmail.com>
2025-08-30 03:56:22 +0530

    Final Commit

---
+++ Practice/threeConsecutiveOdds.java
@@ -0,0 +1,39 @@
+/* 
+
+        1550. Three Consecutive Odds
+        
+        Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+ 
+
+Example 1:
+
+Input: arr = [2,6,4,1]
+Output: false
+Explanation: There are no three consecutive odds.
+Example 2:
+
+Input: arr = [1,2,34,3,4,5,7,23,12]
+Output: true
+Explanation: [5,7,23] are three consecutive odds.*/
+
+
+public class threeConsecutiveOdds {
+    public static boolean threeConse(int arr[]){
+        int check=0;
+        for(int i=0;i<arr.length;i++){
+            if((arr[i])%2!=0){
+                check++;
+                if(check>=3){
+                    return true;
+                }
+            }else{
+                check=0;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int num[] ={2,6,4,1};
+        System.out.println(threeConse(num));
+    }
+}
